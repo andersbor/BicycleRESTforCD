aabf758ee8350f433f919d41e59cf6e6a44e9434
andersbor <anbo@zealand.dk>
2023-11-06 09:40:15 +0100

    wheels

--- BicycleRESTforCD/Models/Bicycle.cs
+++ BicycleRESTforCD/Models/Bicycle.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string? Color { get; set; }
+        public int NumberOfWheels { get; set; }
 
         public void ValidateColor()
         {
